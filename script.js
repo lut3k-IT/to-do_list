bcf690ae712b82d64367479fd02c946c0d390d9c
Kacper Lutyński <lut3k777@gmail.com>
2022-05-31 20:57:01 +0200

    cleaning the code (+transitions)

--- script.js
+++ script.js
@@ -1,28 +1,9 @@
 'use strict';
-
-// ## Funkcjonalność
-// Aplikacja wyświetla listę zadań do wykonania. Zadania podzielone są na *wykonane* oraz *do zrobienia*.
-//   - Zadania *wykonane* są przekreślone i są oznaczone kolorem (`#9eb2c0`) i zaznaczonym polem wyboru po lewej stronie.
-//   - Zadania *do zrobienia* są oznaczone kolorem (`#2e3641`) i niezaznaczonym polem wyboru po lewej stronie.
-//   - Pod listą zadań zawsze wyświetla się pole z możliwością dodania nowego zadania. Nowe zadanie jest zawsze *do zrobienia*
-//   - Po dodaniu nowego zadania, dodawane jest ono nad polem dodawana nowego zadania. Pole z możliwością dodania nowego zadania jest zawsze na samym dole listy.
-//   - Nie można dodać zadania bez wpisania tytułu (trzeba dodać wizualną walidację).
-//   - Każde zadanie można usunąć poprzez kliknięcie w ikonę kosza.
-
-//   - W prawym górnym rogu dodajmy przycisk, który pozwola usunąć wszystkie taski.
-
-//   - **Uwaga!** Aktualny stan listy zapisany jest w LocalStorage.
-//   - Jeśli mamy zapisaną w LocalStorage liste, powinna się ona pojawić od razu po załadowaniu skryptu
-
-//              _           _
-//    ___  ___| | ___  ___| |_ ___  _ __ ___
-//   / __|/ _ | |/ _ \/ __| __/ _ \| '__/ __|
-//  \__ |  __| |  __| (__| || (_) | |  \__ \
-// |___/\___|_|\___|\___|\__\___/|_|  |___/
-
-// const checkbox = document.querySelector('input[type=checkbox]');
-// console.log(checkbox);
-// console.log(checkbox.checked);
+//                      _       _     _
+//    __   ____ _ _ __(_) __ _| |__ | | ___ ___
+//   \ \ / / _` | '__| |/ _` | '_ \| |/ _ / __|
+//   \ V | (_| | |  | | (_| | |_) | |  __\__ \
+//   \_/ \__,_|_|  |_|\__,_|_.__/|_|\___|___/
 
 let $todoBtnAdd; // button dodawania taska
 let $todoInput; // znacznik <input> 
@@ -33,6 +14,11 @@ let $todoCont; // kontener całej aplikacji
 let $idNumber = localStorage.getItem('$idNumber') ?? 0; // id ostatnio dodanego taska
 let $inputVal; // zawartość todo
 
+//      _       _ _   _       _ _          _   _
+//    (_)_ __ (_| |_(_) __ _| (_)______ _| |_(_) ___  _ __
+//   | | '_ \| | __| |/ _` | | |_  / _` | __| |/ _ \| '_ \
+//  | | | | | | |_| | (_| | | |/ | (_| | |_| | (_) | | | |
+// |_|_| |_|_|\__|_|\__,_|_|_/___\__,_|\__|_|\___/|_| |_|
 
 const init = () => {
     prepareDOMElements();
@@ -53,8 +39,16 @@ const prepareDOMEvents = () => {
     $todoCont.addEventListener('click', checkClick);
 }
 
+//      __                  _   _
+//    / _|_   _ _ __   ___| |_(_) ___  _ __  ___
+//   | |_| | | | '_ \ / __| __| |/ _ \| '_ \/ __|
+//  |  _| |_| | | | | (__| |_| | (_) | | | \__ \
+// |_|  \__,_|_| |_|\___|\__|_|\___/|_| |_|___/
+
+// it loads all tasks from localStorage
 const loadTasks = () => {
     if ($idNumber > 0) {
+        // requires a better solution
         for (let i = 0; i < $idNumber; i++) {
             const html = localStorage.getItem(`todo-${i}`);
 
@@ -65,12 +59,11 @@ const loadTasks = () => {
     }
 }
 
+// it adds todo in DOM and in localStorage
 const addTodo = () => {
     if ($todoInput.value !== '') {
         $inputVal = $todoInput.value;
-        // localStorage.setItem(${})
         $idNumber = localStorage.getItem('$idNumber') ?? 0;
-        $todoInput.placeholder = '';
 
         const html = `
         <div id="todo-${$idNumber}" class="todo__list-item">
@@ -83,10 +76,13 @@ const addTodo = () => {
             </button>
         </div>
         `;
-
         $todoList.insertAdjacentHTML('beforeend', html);
+
+        // clearing placeholder & input value
+        $todoInput.placeholder = '';
         $todoInput.value = '';
 
+        // adding task and idNumber to localStorage
         localStorage.setItem(`todo-${$idNumber}`, html);
         $idNumber++;
         localStorage.setItem('$idNumber', $idNumber);
@@ -96,43 +92,42 @@ const addTodo = () => {
     }
 }
 
+// it deletes only selected task in DOM and in localStorage
 const deleteTask = e => {
     const task = e.target.closest('.todo__list-item');
     localStorage.removeItem(task.id);
     task.remove();
 }
 
-const deleteAllTasks = e => {
-    // here
+// it deletes all tasks in DOM and in localStorage
+const deleteAllTasks = () => {
+    const allTasks = document.querySelectorAll('.todo__list-item');
+    localStorage.clear();
+    allTasks.forEach(task => {
+        task.remove();
+    });
 }
 
+// if ENTER is pressed, then addTodo()
 const isEnter = e => {
     if (e.keyCode === 13) {
         addTodo();
     }
 }
 
+// if button clicked
 const checkClick = e => {
-    // jeżeli jest to input:checkbox
     if (e.target.classList.contains('todo__list-item-btn-check')) {
         e.target.closest('.todo__list-item').classList.toggle('item-checked');
+    }
+    else if (e.target.closest('.todo__header-btn-del')) {
+        deleteAllTasks(e);
     }
     else if (e.target.closest('.todo__list-item-btn-del')) {
         deleteTask(e);
     }
 }
 
-//                         _        _ _     _
-//   _____   _____ _ __  | |_     | (_)___| |_ ___ _ __   ___ _ __ ___
-//  / _ \ \ / / _ | '_ \| _______| | / __| __/ _ | '_ \ / _ | '__/ __|
-// |  __/\ V |  __| | | | ||_____| | \__ | ||  __| | | |  __| |  \__ \
-// \___| \_/ \___|_| |_|\__|    |_|_|___/\__\___|_| |_|\___|_|  |___/
 
-
-//      __                  _   _
-//    / _|_   _ _ __   ___| |_(_) ___  _ __  ___
-//   | |_| | | | '_ \ / __| __| |/ _ \| '_ \/ __|
-//  |  _| |_| | | | | (__| |_| | (_) | | | \__ \
-// |_|  \__,_|_| |_|\___|\__|_|\___/|_| |_|___/
-
+// do stuff after DOMContentLoaded
 document.addEventListener('DOMContentLoaded', init);
