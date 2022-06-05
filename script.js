5499860707408aba21d4ab2969959d2154b6457d
Kacper Lutyński <lut3k777@gmail.com>
2022-06-05 12:08:24 +0200

    OOP implemented

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 'use strict';
-//                      _       _     _
-//    __   ____ _ _ __(_) __ _| |__ | | ___ ___
-//   \ \ / / _` | '__| |/ _` | '_ \| |/ _ / __|
-//   \ V | (_| | |  | | (_| | |_) | |  __\__ \
-//   \_/ \__,_|_|  |_|\__,_|_.__/|_|\___|___/
+//         _       _           _
+//    __ _| | ___ | |__   __ _| |    __   ____ _ _ __
+//  / _` | |/ _ \| '_ \ / _` | |____\ \ / / _` | '__|
+// | (_| | | (_) | |_) | (_| | |_____\ V | (_| | |
+// \__, |_|\___/|_.__/ \__,_|_|      \_/ \__,_|_|
+// |___/
 
-let $todoBtnAdd; // button dodawania taska
-let $todoInput; // znacznik <input> 
-let $todoList; // kontener na taski
-let $newTask; // nowo dodany task
-let $todoCont; // kontener całej aplikacji
+// DOM
+let $todoBtnAdd; // adding task button
+let $todoInput; // <input> tag
+let $todoCont; // whole app container
+let $todoList; // tasks container
 
-let $idNumber = localStorage.getItem('$idNumber') ?? 0; // id ostatnio dodanego taska
-let $inputVal; // zawartość todo
+// storage
+let $tasks = []; // all tasks array
+
+//        _
+//    ___| | __ _ ___ ___  ___ ___
+//  / __| |/ _` / __/ __|/ _ / __|
+// | (__| | (_| \__ \__ |  __\__ \
+// \___|_|\__,_|___|___/\___|___/
+
+class Todo {
+    constructor(content, isChecked = false) {
+        this.id = (Date.now() + '').slice(-10);
+        this.content = content;
+        this.isChecked = isChecked;
+    }
+};
 
 //      _       _ _   _       _ _          _   _
 //    (_)_ __ (_| |_(_) __ _| (_)______ _| |_(_) ___  _ __
@@ -34,9 +49,10 @@ const prepareDOMElements = () => {
 }
 
 const prepareDOMEvents = () => {
-    $todoBtnAdd.addEventListener('click', addTodo);
-    $todoInput.addEventListener('keyup', isEnter);
+    // buttons listener
     $todoCont.addEventListener('click', checkClick);
+    // ENTER key listener
+    $todoInput.addEventListener('keyup', isEnter);
 }
 
 //      __                  _   _
@@ -45,89 +61,154 @@ const prepareDOMEvents = () => {
 //  |  _| |_| | | | | (__| |_| | (_) | | | \__ \
 // |_|  \__,_|_| |_|\___|\__|_|\___/|_| |_|___/
 
-// it loads all tasks from localStorage
-const loadTasks = () => {
-    if ($idNumber > 0) {
-        // requires a better solution
-        for (let i = 0; i < $idNumber; i++) {
-            const html = localStorage.getItem(`todo-${i}`);
+// SPIS
+// ===
+// createTask - tworzy obiekt
+// addTaskDOM - wrzuca wizualnie taska (DOM)
+// addTasksLS - zapisuje wszystkie taski w localStorage
+// clearInput - czyści inputa
+// deleteTask - usuwa taska
+// deleteAllTasks - usuwa wszystkie taski
+// isEnter - sprawdza czy naciśnięto ENTER na polu input
 
-            if (html !== null) {
-                $todoList.insertAdjacentHTML('beforeend', html);
-            }
-        }
-    }
-}
 
-// it adds todo in DOM and in localStorage
-const addTodo = () => {
+// it creates a todo object
+const createTask = () => {
     if ($todoInput.value !== '') {
-        $inputVal = $todoInput.value;
-        $idNumber = localStorage.getItem('$idNumber') ?? 0;
+        const inputVal = $todoInput.value; // what user wrote
+        const todo = new Todo(inputVal); // todo object
 
-        const html = `
-        <div id="todo-${$idNumber}" class="todo__list-item">
-            <div class="todo__list-item-btn">
-                <input class="todo__list-item-btn-check" type="checkbox">
-            </div>
-            <div class="todo__list-item-content">${$inputVal}</div>
-            <button class="todo__list-item-btn  todo__list-item-btn-del">
-                <img src="img/trash.svg" alt="trash">
-            </button>
-        </div>
-        `;
-        $todoList.insertAdjacentHTML('beforeend', html);
+        // adding the task to array, DOM and localStorage
+        $tasks.push(todo);
+        addTaskDOM(todo);
+        addTasksLS();
 
-        // clearing placeholder & input value
-        $todoInput.placeholder = '';
-        $todoInput.value = '';
-
-        // adding task and idNumber to localStorage
-        localStorage.setItem(`todo-${$idNumber}`, html);
-        $idNumber++;
-        localStorage.setItem('$idNumber', $idNumber);
+        // clearing the placeholder & input value
+        clearInput();
     }
     else {
         $todoInput.placeholder = 'Uzupełnij pole!';
     }
 }
 
-// it deletes only selected task in DOM and in localStorage
+// it loads all tasks from localStorage and delegate rendering taks in DOM
+const loadTasks = () => {
+    if (!localStorage.getItem('tasks')) return;
+    $tasks = JSON.parse(localStorage.getItem('tasks'));
+
+    if (!$tasks) return;
+    $tasks.forEach(task => addTaskDOM(task));
+}
+
+// it adds todo into DOM
+const addTaskDOM = todo => {
+    let html;
+
+    if (todo.isChecked === false) {
+        html = `
+        <div data-id="${todo.id}" class="todo__list-item">
+            <div class="todo__list-item-btn">
+                <input class="todo__list-item-btn-check" type="checkbox">
+            </div>
+            <div class="todo__list-item-content">${todo.content}</div>
+            <button class="todo__list-item-btn  todo__list-item-btn-del">
+                <img src="img/trash.svg" alt="trash">
+            </button>
+        </div>
+        `;
+    }
+    else {
+        html = `
+        <div data-id="${todo.id}" class="todo__list-item item-checked">
+            <div class="todo__list-item-btn">
+                <input class="todo__list-item-btn-check" type="checkbox" checked>
+            </div>
+            <div class="todo__list-item-content">${todo.content}</div>
+            <button class="todo__list-item-btn  todo__list-item-btn-del">
+                <img src="img/trash.svg" alt="trash">
+            </button>
+        </div>
+        `;
+    }
+
+    $todoList.insertAdjacentHTML('beforeend', html);
+}
+
+// it saves the tasks in localStorage
+const addTasksLS = () => {
+    localStorage.setItem('tasks', JSON.stringify($tasks));
+}
+
+// it clears the input
+const clearInput = () => {
+    $todoInput.placeholder = '';
+    $todoInput.value = '';
+}
+
+// it deletes only selected task from DOM and localStorage
 const deleteTask = e => {
     const task = e.target.closest('.todo__list-item');
-    localStorage.removeItem(task.id);
+    const taskID = task.dataset.id;
+    $tasks = $tasks.filter(task => task.id !== taskID);
+
+    // deleting & adding tasks - localStorage
+    localStorage.removeItem('tasks');
+    addTasksLS();
+
+    // deleting task - DOM
     task.remove();
 }
 
-// it deletes all tasks in DOM and in localStorage
+// it deletes all tasks from DOM and localStorage
 const deleteAllTasks = () => {
-    const allTasks = document.querySelectorAll('.todo__list-item');
     localStorage.clear();
+
+    const allTasks = document.querySelectorAll('.todo__list-item');
     allTasks.forEach(task => {
         task.remove();
     });
 }
 
-// if ENTER is pressed, then addTodo()
+// it adds a task if ENTER is pressed
 const isEnter = e => {
     if (e.keyCode === 13) {
-        addTodo();
+        createTask();
     }
 }
 
 // if button clicked
 const checkClick = e => {
+    // checkbox
     if (e.target.classList.contains('todo__list-item-btn-check')) {
-        e.target.closest('.todo__list-item').classList.toggle('item-checked');
+        const taskDOM = e.target.closest('.todo__list-item');
+        taskDOM.classList.toggle('item-checked');
+
+        // changing isChecked
+        $tasks.forEach(task => {
+            if (taskDOM.dataset.id === task.id) {
+                if (task.isChecked === false) task.isChecked = true;
+                else if (task.isChecked === true) task.isChecked = false;
+            }
+        });
+
+        // updating localStorage
+        addTasksLS();
     }
+    // delete one
+    else if (e.target.closest('.todo__list-item-btn-del')) {
+        deleteTask(e);
+    }
+    // delete all
     else if (e.target.closest('.todo__header-btn-del')) {
         deleteAllTasks(e);
     }
-    else if (e.target.closest('.todo__list-item-btn-del')) {
-        deleteTask(e);
+    // input
+    else if (e.target.closest('.todo__insert-btn')) {
+        createTask();
     }
 }
 
 
 // do stuff after DOMContentLoaded
+//////////////////////////////////
 document.addEventListener('DOMContentLoaded', init);
